f99d6ac8e7419f4e193ae92b54ddf501be3002ee
Aleksei Baliuk <aleksred.b@gmail.com>
2023-01-27 14:28:17 +0200

    fix

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -9,21 +9,25 @@ const updateAvatar = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
   const { _id: id } = req.user;
 
-  const [extension] = originalname.split('.').reverse();
+  const [extension] = originalname.split(".").reverse();
   const avatarName = `${id}.${extension}`;
 
   // const avatarName = `${id}_${originalname}`;
 
-  await Jimp.read(tempUpload)
-    .then((avatar) => {
-      avatar.resize(250, 250);
-      avatar.write(tempUpload);
-    })
-    .catch((err) => {
-      throw err;
-    });
+  // await Jimp.read(tempUpload)
+  //   .then((avatar) => {
+  //     avatar.resize(250, 250);
+  //     avatar.write(tempUpload);
+  //   })
+  //   .catch((err) => {
+  //     throw err;
+  //   });
 
   try {
+    const avatar = await Jimp.read(tempUpload);
+    avatar.resize(250, 250);
+    avatar.write(tempUpload);
+
     const resultUpload = path.join(avatarsDir, avatarName);
     await fs.rename(tempUpload, resultUpload);
     const avatarURL = path.join("avatars", avatarName);
